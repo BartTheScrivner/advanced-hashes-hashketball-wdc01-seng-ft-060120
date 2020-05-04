4877addc2b55a5a0b924e854078735aadaedcddc
Dave Frame <dframe22@gmail.com>
2020-05-04 22:31:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,3 +172,15 @@ def player_numbers(team_name)
   number_hash 
 end
 
+def player_numbers(team_name)
+  number_hash = []
+  game_hash.each_pair do |(team, info)|
+    if info[:team_name] == team_name
+      info[:players].count do |player|
+        number_hash.push(player[:number])
+      end
+    end
+  end
+  number_hash 
+end
+
